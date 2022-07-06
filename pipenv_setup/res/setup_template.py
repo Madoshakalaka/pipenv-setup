0c55f287ece2af691987727a4c58a89b1e93f202
Matt Yan <syan4@ualberta.ca>
2022-07-07 01:59:16 +0800

    remove outdated links

--- pipenv_setup/res/setup_template.py
+++ pipenv_setup/res/setup_template.py
@@ -1,10 +1,3 @@
-"""A setuptools based setup module.
-See:
-https://packaging.python.org/guides/distributing-packages-using-setuptools/
-https://github.com/pypa/sampleproject
-Modified by Madoshakalaka@Github (dependency links added)
-"""
-
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 from os import path
